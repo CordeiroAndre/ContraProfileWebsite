1241aca477aa4aa8fa01eca372ff826a38f5bce3
Andre <60711778+CordeiroAndre@users.noreply.github.com>
2022-02-13 21:57:19 -0300

    Update

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ setTimeout(function(){
 function removeElementsByClass(className){
     
     var iframe = document.getElementById("computer3d");
+    console.log(iframe);
     const elements = iframe.contentWindow.document.getElementsByClassName(className);
-    console.log(elements.length)
+    console.log(elements)
 
     while(elements.length > 0){
         elements[0].parentNode.removeChild(elements[0]);
