937fba98a27e9eb2db80a0648d91ba6e7de196b9
Andre <60711778+CordeiroAndre@users.noreply.github.com>
2022-02-13 22:21:03 -0300

    Update

--- script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    let myiFrame = document.getElementById("computer3d");
-    console.log(myiFrame)
-    let doc = myiFrame.contentWindow;
-    console.log(doc)
-    doc.body.innerHTML = doc.body.innerHTML + '<style> .spline-watermark img{display: none !important;}</style>';
- }
