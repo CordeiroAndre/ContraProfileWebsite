ac6615bf86490a0687f88f9f2078c1703f652aa6
Andre <60711778+CordeiroAndre@users.noreply.github.com>
2022-11-27 14:40:13 -0300

    Collapsed most of the code to an isolated function

--- App.js
+++ App.js
@@ -6,32 +6,37 @@ document.getElementById('main-title').innerText = data["Main-Title"];
 document.getElementById('main-description').innerText = data["Main-Description"];
 
 
-var professionalProjectsList = document.getElementById("professional-projects");
-data["Professional-Projects"].forEach(project => {
-    var projectListElement = document.createElement("li");
-    
-    var projectTitle = document.createElement("span");
-    projectTitle.classList.add('mark-pink-light');
-    projectTitle.innerText = project.Title;
-    projectListElement.appendChild(projectTitle);
+function createListItem(unordedListID, JsonFieldName, CssClassMark) {
+    var professionalProjectsList = document.getElementById(unordedListID);
+    data[JsonFieldName].forEach(project => {
+        var projectListElement = document.createElement("li");
 
-    projectListElement.appendChild(document.createElement("br"));
+        var projectTitle = document.createElement("span");
+        projectTitle.classList.add(CssClassMark);
+        projectTitle.innerText = project.Title;
+        projectListElement.appendChild(projectTitle);
 
-    var projectDescription = document.createElement("span");
-    projectDescription.innerText = project.Description;
-    projectListElement.appendChild(projectDescription);
+        projectListElement.appendChild(document.createElement("br"));
 
-    var tagsWrapper = document.createElement("div");
-    tagsWrapper.classList.add('tags-wrapper');
-    projectListElement.appendChild(tagsWrapper);
+        var projectDescription = document.createElement("span");
+        projectDescription.innerText = project.Description;
+        projectListElement.appendChild(projectDescription);
 
-    project.Tags.forEach(Tag => {
-        var tag = document.createElement("img");
-        tag.src = tagBadge.replace('{property}', Tag);
-        tagsWrapper.appendChild(tag);
+        var tagsWrapper = document.createElement("div");
+        tagsWrapper.classList.add('tags-wrapper');
+        projectListElement.appendChild(tagsWrapper);
+
+        project.Tags.forEach(Tag => {
+            var tag = document.createElement("img");
+            tag.src = tagBadge.replace('{property}', Tag);
+            tagsWrapper.appendChild(tag);
+        });
+
+        professionalProjectsList.appendChild(projectListElement);
     });
 
+}
 
-    professionalProjectsList.appendChild(projectListElement);
-});
+createListItem('professional-projects', 'Professional-Projects', 'mark-pink-light');
+createListItem('education', 'Education', 'mark-green-light');
 
